a2b76f63d1609116f6dcc2de843dd272edbf0575
Ahad <ahadbutt22001@gmail.com>
2025-12-18 08:19:14 +0500

    Update Bubble_sort.cpp

--- DSA/Bubble_sort.cpp
+++ DSA/Bubble_sort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void bubble_sort(int arr[],int size){
-	for (int i=0;i<size;i++){
+	for (int i=0;i<size-1;i++){
 		for (int j=0 ; j<size-i-1 ; j++){
 			if(arr[j]>arr[j+1])
 			{
@@ -33,3 +33,4 @@ int main(){
 	
 	return 0;	
 }
+
